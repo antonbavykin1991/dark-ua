f061dbdd81923f252d7f60009b7b758b3faa086b
Anton Bavykin <antonbavykin1991@gmail.com>
2019-03-10 23:17:30 +0200

    fixed checkout

--- routes/admin.js
+++ routes/admin.js
@@ -425,7 +425,7 @@ router.post('/admin/file/upload', common.restrict, common.checkAccess, upload.si
 
         // Get the mime type of the file
         const mimeType = mime.lookup(file.originalname);
-        
+
         // Check for allowed mime type and file size
         if(!common.allowedMimeType.includes(mimeType) || file.size > common.fileSizeLimit){
             // Remove temp file
